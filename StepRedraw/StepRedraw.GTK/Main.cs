2f79c2bb97d075d97b2d4c9844174274aa7a81a0
Serhii Michkovskyi <michkovsky@gmail.com>
2019-06-26 10:38:50 +0300

    Add GTK target project and consolidate framework versions

---
+++ StepRedraw/StepRedraw.GTK/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace StepRedraw.GTK
+{
+    public class Program
+    {
+        // This is the main entry point of the application.
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Step Redraw");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
